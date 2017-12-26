b1ccbe33cf6c7a686de7faa0737a9283e5c7172d
Przemek Jurkiewicz <hankusekbb@gmail.com>
2017-12-26 16:00:07 +0100

    add index.html & scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+var a = 3;
+	b = 2;
+	value = (a * a) - (2 * a * b) + (b * b);
+
+if (value > 0) {
+	console.log ('Wynik dodatni')
+}
+if else (value < 0) {
+	console.log ('Wynik ujemny')
+}
+if else (value = 0) {
+	console.log ('twój wynik to 0')
+}
